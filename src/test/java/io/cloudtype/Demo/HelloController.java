41e248703d91c545c4581d23c674e8e8a39dd4f3
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-08 21:34:03 +0900

    Update HelloController.java

--- src/test/java/io/cloudtype/Demo/HelloController.java
+++ src/test/java/io/cloudtype/Demo/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloController {
 
     @GetMapping("/hello")
