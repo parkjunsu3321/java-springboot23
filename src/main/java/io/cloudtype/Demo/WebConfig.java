75a17184b75e276bd3c7a2d70d6a7a987f61d209
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 15:30:02 +0900

    Update WebConfig.java

--- src/main/java/io/cloudtype/Demo/WebConfig.java
+++ src/main/java/io/cloudtype/Demo/WebConfig.java
@@ -1,4 +1,10 @@
 package io.cloudtype.Demo;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 ⠀⠀
