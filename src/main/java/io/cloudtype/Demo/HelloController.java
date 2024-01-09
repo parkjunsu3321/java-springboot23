010b4e7cf7ab70079ced09acb3139d7a8afdb2fe
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 14:54:55 +0900

    Update HelloController.java

--- src/main/java/io/cloudtype/Demo/HelloController.java
+++ src/main/java/io/cloudtype/Demo/HelloController.java
@@ -1,7 +1,7 @@
 package io.cloudtype.Demo;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*")
 @RestController
