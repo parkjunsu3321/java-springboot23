dce3d031195fc7db5cc7510cdf65acf920546fe9
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-09 15:24:52 +0900

    Create WebConfig.java

---
+++ src/main/java/io/cloudtype/Demo/WebConfig.java
@@ -0,0 +1,18 @@
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+⠀⠀
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*") // 안에 해당 주소를 넣어도 됨
+                        .allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS" , "PATCH")
+                        .exposedHeaders("Authorization", "RefreshToken");
+                //.allowCredentials(true); // .allowedOriginPatterns("*") 이렇게 와일드 카드로 설정하면 이거 쓰면 에러남 ( 실행 조차  X )
+            }
+        };
+    }
+}
