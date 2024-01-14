3655fae75dfc9d045043f027f7a843795dc34ce8
parkjunsu3321 <127470862+parkjunsu3321@users.noreply.github.com>
2024-01-14 18:19:41 +0900

    Update ReactionController.java

--- src/main/java/io/cloudtype/Demo/ReactionController.java
+++ src/main/java/io/cloudtype/Demo/ReactionController.java
@@ -24,7 +24,8 @@ public class ReactionController {
     @PostMapping("/login")
     public String login(@RequestParam(name = "id", required = false) String id, @RequestParam(name = "pass", required = false) String pass) 
     {
-        return "Login successful!";
+        PostgreSQLConnection psc = new PostgreSQLConnection(id, pass);
+        return psc.Login();
     }
 
     @GetMapping("/db")
